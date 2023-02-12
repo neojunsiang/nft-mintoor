5f330bb15a9450ec9fea740b42c9b775c12d90c8
neojunsiang <neo_jun_siang@hotmail.com>
2023-02-12 15:51:36 +0800

    added sugar launch and token creation

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,4 +1,11 @@
-import { FC } from "react";
+import {
+  FC,
+  MouseEventHandler,
+  useCallback,
+  useMemo,
+  useState,
+  useEffect,
+} from "react";
 import {
   Button,
   Container,
@@ -9,8 +16,72 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+import { PublicKey } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Metaplex,
+  walletAdapterIdentity,
+  CandyMachine,
+  CandyMachineV2,
+} from "@metaplex-foundation/js";
 
 const Connected: FC = () => {
+  const { connection } = useConnection();
+  const walletAdapter = useWallet();
+  const [candyMachine, setCandyMachine] = useState<CandyMachineV2>();
+  const [isMinting, setIsMinting] = useState(false);
+
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter));
+  }, [connection, walletAdapter]);
+
+  useEffect(() => {
+    if (!metaplex) {
+      return;
+    }
+    metaplex
+      .candyMachinesV2()
+      .findByAddress({
+        address: new PublicKey("5CfVaA1HwTpR2vvpG1z3a4wrbmt8AibLvnPCCkBcYman"),
+      })
+      .then((candyMachine) => {
+        console.log("candyMachine", candyMachine);
+        setCandyMachine(candyMachine);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [metaplex]);
+
+  const router = useRouter();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+      if (event.defaultPrevented) {
+        return;
+      }
+
+      if (!walletAdapter.connected || !candyMachine) {
+        return;
+      }
+
+      try {
+        setIsMinting(true);
+        const nft = await metaplex.candyMachinesV2().mint({
+          candyMachine,
+        });
+        console.log("nft", nft);
+        router.push(`/newMint?mint=${nft.nft.address.toBase58()}`);
+      } catch (err) {
+        alert(err);
+      } finally {
+        setIsMinting(false);
+      }
+    },
+    [walletAdapter.connected, candyMachine, metaplex, router]
+  );
+
   return (
     <VStack spacing={20}>
       <Container>
@@ -41,7 +112,13 @@ const Connected: FC = () => {
         <Image src="avatar5.png" alt="" />
       </HStack>
 
-      <Button bgColor="accent" color={"white"} maxW="380px">
+      <Button
+        bgColor="accent"
+        color={"white"}
+        maxW="380px"
+        onClick={handleClick}
+        isLoading={isMinting}
+      >
         <HStack>
           <Text>mint buildoor</Text>
           <ArrowForwardIcon />
